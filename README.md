86b46fec013ab09882c5c292a5f4db97509229ad
Troy Neubauer <troyneubauer@gmail.com>
2021-09-03 17:22:07 -0400

    Update description

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # minimal-toml
 
+A no_std toml deserializer for embedded systems
+Full toml support for deserializing into structs that implement [`serde::Deserialize`].
+
+Requires no memory allocations and is likely much faster than toml-rs.
+Supports deserialization only
+
+
 License: MIT
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
+//! A no_std toml deserializer for embedded systems
+//! Full toml support for deserializing into structs that implement [`serde::Deserialize`].
+//!
+//! Requires no memory allocations and is likely much faster than toml-rs.
+//! Supports deserialization only
+//!
+#![no_std]
 
